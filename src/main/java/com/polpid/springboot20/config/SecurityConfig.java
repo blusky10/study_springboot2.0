66c6d38941ebc2533fb9566d0a2633c08a3ae304
blusky0910@gmail.com <blusky0910@gmail.com>
2019-02-15 17:50:28 +0900

    Update: security 설정 추가

--- src/main/java/com/polpid/springboot20/config/SecurityConfig.java
+++ src/main/java/com/polpid/springboot20/config/SecurityConfig.java
@@ -12,7 +12,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/forgot").permitAll();
+                .antMatchers("/", "/forgot", "/h2-console/**").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
 
     }
 }
